30298ef628ae8264b09040f390c4cf39e0741a09
OraPro Ltd <david@drewette.com>
2022-05-23 10:27:15 +0100

    Update APEX_workspace_ID_fix.sql

--- apex/APEX_workspace_ID_fix.sql
+++ apex/APEX_workspace_ID_fix.sql
@@ -6,9 +6,9 @@ begin
   apex_application_install.set_workspace_id (v_workspace_id);
   apex_util.set_security_group_id
     (p_security_group_id => apex_application_install.get_workspace_id);
-  apex_application_install.set_schema('APX_FLASK_CONFIGURATION');
+  apex_application_install.set_schema('APP_SCHEMA');
   apex_application_install.set_application_id(1530);
   apex_application_install.generate_offset;
-  apex_application_install.set_application_alias('FLASK_CONFIGURATION');
+  apex_application_install.set_application_alias('APP_ALIAS');
 end;
  /
